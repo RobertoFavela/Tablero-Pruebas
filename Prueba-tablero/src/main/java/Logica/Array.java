6d945a6e4c28d125ae70bdde4c5ea7f10d61b39d
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2024-10-14 12:05:01 -0600

    Colocar ficha arreglado

--- Prueba-tablero/src/main/java/Logica/Array.java
+++ Prueba-tablero/src/main/java/Logica/Array.java
@@ -53,31 +53,36 @@ public class Array {
 //        }
         if (ficha.getLado1() == extremoIzquierdo) {
             System.out.println("entraste al primer if");
-            tablero[extremo1Fila][extremo1Columna + 1] = ficha.getLado1();
-            tablero[extremo1Fila][extremo1Columna + 2] = ficha.getLado2();
+            tablero[extremo1Fila][extremo1Columna - 1] = ficha.getLado1();
+            tablero[extremo1Fila][extremo1Columna - 2] = ficha.getLado2();
 
             extremoIzquierdo = ficha.getLado2();
-            return true;
-        }
-        if (ficha.getLado2() == extremoDerecha) {
-            System.out.println("entraste al segundo if");
-            tablero[extremo2Fila][extremo2Columna + 1] = ficha.getLado2();
-            tablero[extremo2Fila][extremo2Columna + 2] = ficha.getLado1();
-            extremoDerecha = ficha.getLado1();
+            extremo1Columna -= 2;
             return true;
         }
         if (ficha.getLado1() == extremoDerecha) {
-            System.out.println("entraste al tercer if");
-            tablero[extremo2Fila][extremo2Columna - 1] = ficha.getLado1();
-            tablero[extremo2Fila][extremo2Columna - 2] = ficha.getLado2();
-            extremoIzquierdo = ficha.getLado2();
+            System.out.println("entraste al segundo if");
+            tablero[extremo2Fila][extremo2Columna + 1] = ficha.getLado1();
+            tablero[extremo2Fila][extremo2Columna + 2] = ficha.getLado2();
+            extremoDerecha = ficha.getLado2();
+            extremo2Columna += 2;
             return true;
         }
         if (ficha.getLado2() == extremoIzquierdo) {
-            System.out.println("entraste al cuarto if");
+            System.out.println("entraste al tercer if");
             tablero[extremo1Fila][extremo1Columna - 1] = ficha.getLado2();
             tablero[extremo1Fila][extremo1Columna - 2] = ficha.getLado1();
+
+            extremoIzquierdo = ficha.getLado1();
+            extremo1Columna -= 2;
+            return true;
+        }
+        if (ficha.getLado2() == extremoDerecha) {
+            System.out.println("entraste al cuarto if");
+            tablero[extremo2Fila][extremo2Columna + 1] = ficha.getLado2();
+            tablero[extremo2Fila][extremo2Columna + 2] = ficha.getLado1();
             extremoDerecha = ficha.getLado1();
+            extremo2Columna += 2;
             return true;
         }
         return false;
