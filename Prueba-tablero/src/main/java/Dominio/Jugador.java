a4ed2a7bedac240d73e1fd0c70e0d3272c9fd7d3
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2024-11-06 18:17:14 -0700

    Comentarios

--- Prueba-tablero/src/main/java/Dominio/Jugador.java
+++ Prueba-tablero/src/main/java/Dominio/Jugador.java
@@ -14,55 +14,47 @@ import java.util.List;
  */
 public class Jugador {
 
-    private String nombre;
-    private List<Ficha> fichas; // Lista para guardar las fichas del jugador
+      private String nombre;
+      private List<Ficha> fichas;
 
-    // Constructor
-    public Jugador(String nombre) {
-        this.nombre = nombre;
-        this.fichas = new ArrayList<>();
-    }
+      public Jugador(String nombre) {
+            this.nombre = nombre;
+            this.fichas = new ArrayList<>();
+      }
 
-    // Método para obtener el nombre del jugador
-    public String getNombre() {
-        return nombre;
-    }
+      public String getNombre() {
+            return nombre;
+      }
 
-    // Método para establecer el nombre del jugador
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+      public void setNombre(String nombre) {
+            this.nombre = nombre;
+      }
 
-    // Método para obtener la lista de fichas del jugador
-    public List<Ficha> getFichas() {
-        return fichas;
-    }
+      public List<Ficha> getFichas() {
+            return fichas;
+      }
 
-    // Método para agregar una ficha a la lista del jugador
-    public void agregarFicha(Ficha ficha) {
-        this.fichas.add(ficha);
-    }
+      public void agregarFicha(Ficha ficha) {
+            this.fichas.add(ficha);
+      }
 
-    // Método para eliminar una ficha de la lista del jugador
-    public boolean eliminarFicha(Ficha ficha) {
-        if (this.fichas.contains(ficha)) {
-            this.fichas.remove(ficha);
-            return true;
-        } else {
-            return false; 
-        }
-    }
+      public boolean eliminarFicha(Ficha ficha) {
+            if (this.fichas.contains(ficha)) {
+                  this.fichas.remove(ficha);
+                  return true;
+            } else {
+                  return false;
+            }
+      }
 
-    // Método para agregar varias fichas a la lista del jugador
-    public void agregarFichas(List<Ficha> fichas) {
-        this.fichas.addAll(fichas);
-    }
+      public void agregarFichas(List<Ficha> fichas) {
+            this.fichas.addAll(fichas);
+      }
 
-    // Método para mostrar las fichas del jugador
-    public void mostrarFichas() {
-        for (Ficha ficha : fichas) {
-            System.out.println(ficha);
-        }
-    }
+      public void mostrarFichas() {
+            for (Ficha ficha : fichas) {
+                  System.out.println(ficha);
+            }
+      }
 
 }
